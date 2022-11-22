62cab58c207963e122887f29d06e85a9c58f111c
Max Pugh <mpugh@chapman.edu>
2022-11-22 11:55:23 -0700

    edited select individual function

--- project2.py
+++ project2.py
@@ -14,50 +14,38 @@ def sort_population_by_fitness(population):
 
 
 # ///////////////////////////////////////////////////////////////
-# finish functions
+
 def select_individual(sorted_population, fitness_sum):
-    offset = 0
-    normalized_fitness_sum = fitness_sum
-
-    lowest_fitness = objective_function(sorted_population[0])
-    if lowest_fitness < 0:
-        offset = -lowest_fitness
-        normalized_fitness_sum += offset * len(sorted_population)
-
-    draw = random.uniform(0, 1)
-
-    accumulated = 0
+    rand = random.uniform(0, fitness_sum)
+    current_sum = 0
     for individual in sorted_population:
-        fitness = objective_function(individual) + offset
-        probability = fitness / normalized_fitness_sum
-        accumulated += probability
-
-        if draw <= accumulated:
+        current_sum += objective_function(individual)
+        if current_sum > rand:
             return individual
+    return sorted_population[-1]
 
+   
 
+# this function is used to crossover two individuals to produce a new individual
 def crossover(individual_a, individual_b):
-    xa = individual_a["x"]
-    ya = individual_a["y"]
-
-    xb = individual_b["x"]
-    yb = individual_b["y"]
-
-    return {"x": (xa + xb) / 2, "y": (ya + yb) / 2}
+    next_x = (individual_a["x"] + individual_b["x"]) / 2
+    next_y = (individual_a["y"] + individual_b["y"]) / 2
+    return {"x": next_x, "y": next_y}
+    
 
 
 
 
 def mutate(individual):
-    mutation_rate = 0.01
-    next_x=individual["x"]+ random.uniform(-mutation_rate, mutation_rate)
-    next_y=individual["y"]+ random.uniform(-mutation_rate, mutation_rate)
     lower_boundary, upper_boundary = (-4, 4)
 
-    next_x = min(max(next_x, lower_boundary), upper_boundary)
-    next_y = min(max(next_y, lower_boundary), upper_boundary)
-    
+    mutation_rate = 0.05
 
+    next_x=individual["x"]+ random.uniform(-mutation_rate, mutation_rate)
+    next_x = min(max(next_x, lower_boundary), upper_boundary)
+
+    next_y=individual["y"]+ random.uniform(-mutation_rate, mutation_rate)
+    next_y = min(max(next_y, lower_boundary), upper_boundary)
     return {"x": next_x, "y": next_y}
 
 
@@ -73,7 +61,7 @@ def make_next_generation(previous_population):
     for i in range(population_size):
         first_choice = select_individual(sorted_by_fitness_population, fitness_sum)
         second_choice = select_individual(sorted_by_fitness_population, fitness_sum)
-
+    
         individual = crossover(first_choice, second_choice)
         individual = mutate(individual)
         next_generation.append(individual)
@@ -97,17 +85,13 @@ def generate_population(size, x_boundaries, y_boundaries):
 
     return population
 
-generations = 100
+generations = 10
 
 population = generate_population(size=10, x_boundaries=(-4, 4), y_boundaries=(-4, 4))
 
 i = 1
 while True:
-    print(f"🧬 GENERATION {i}")
-
-    for individual in population:
-        print(individual, objective_function(individual))
-
+    
     if i == generations:
         break
 
